949190149190469b085f87f242dbbd86473bb89a
Yousuff <yousuffcse@gmail.com>
2024-04-01 22:30:36 +0530

    Update Optical_Fiber.c

--- Optical_Fiber.c
+++ Optical_Fiber.c
@@ -1,35 +1,48 @@
 #include<stdio.h>
 #include<math.h>
 
+#define MAX_MEASUREMENTS 10 // Maximum number of measurements
+
 int main()
 {
-    float NA1, NA2, NA3, NA4, NA5, NA6, NA7, d1, d2, d3, d4, d5, d6, d7, L1, L2, L3, L4, L5, L6, L7, NA;
+    int num_measurements;
+    float L[MAX_MEASUREMENTS], d[MAX_MEASUREMENTS], NA[MAX_MEASUREMENTS], total_NA = 0;
 
-    printf("Enter seven measured value of The distance from the end of the fiber to the screen(L):\n");
-    scanf("\n%f\n%f\n%f\n%f\n%f\n%f\n%f", &L1, &L2, &L3, &L4, &L5, &L6, &L7);
-    printf("Enter seven measured value of The diameter of the circular spot of light projected from the end of the fiber onto a screen(d):\n");
-    scanf("\n%f\n%f\n%f\n%f\n%f\n%f\n%f", &d1, &d2, &d3, &d4, &d5, &d6, &d7);
+    printf("Enter the number of measurements (up to %d): ", MAX_MEASUREMENTS);
+    scanf("%d", &num_measurements);
 
-    NA1 = d1/(sqrt(pow(d1, 2)+4*pow(L1, 2)));
-    NA2 = d2/(sqrt(pow(d2, 2)+4*pow(L2, 2)));
-    NA3 = d3/(sqrt(pow(d3, 2)+4*pow(L3, 2)));
-    NA4 = d4/(sqrt(pow(d4, 2)+4*pow(L4, 2)));
-    NA5 = d5/(sqrt(pow(d5, 2)+4*pow(L5, 2)));
-    NA6 = d6/(sqrt(pow(d6, 2)+4*pow(L6, 2)));
-    NA7 = d7/(sqrt(pow(d7, 2)+4*pow(L7, 2)));
+    if (num_measurements > MAX_MEASUREMENTS || num_measurements <= 0) {
+        printf("Invalid number of measurements. Exiting...\n");
+        return 1;
+    }
 
-    NA = (NA1+NA2+NA3+NA4+NA5+NA6+NA7)/7;
+    // Input measurements
+    printf("Enter the measured values of the distance from the end of the fiber to the screen (L):\n");
+    for (int i = 0; i < num_measurements; i++) {
+        scanf("%f", &L[i]);
+    }
 
-    printf("The Numerical Aperture of the given optical fiber, NA1= %f\n", NA1);
-    printf("The Numerical Aperture of the given optical fiber, NA2= %f\n", NA2);
-    printf("The Numerical Aperture of the given optical fiber, NA3= %f\n", NA3);
-    printf("The Numerical Aperture of the given optical fiber, NA4= %f\n", NA4);
-    printf("The Numerical Aperture of the given optical fiber, NA5= %f\n", NA5);
-    printf("The Numerical Aperture of the given optical fiber, NA6= %f\n", NA6);
-    printf("The Numerical Aperture of the given optical fiber, NA7= %f\n", NA7);
+    printf("Enter the measured values of the diameter of the circular spot of light projected from the end of the fiber onto a screen (d):\n");
+    for (int i = 0; i < num_measurements; i++) {
+        scanf("%f", &d[i]);
+    }
 
+    // Calculate NA for each measurement
+    for (int i = 0; i < num_measurements; i++) {
+        NA[i] = d[i] / sqrt(pow(d[i], 2) + 4 * pow(L[i], 2));
+        total_NA += NA[i];
+    }
 
-    printf("\nThe Numerical Aperture of the given optical fiber, NA = %f\n\n", NA);
+    // Calculate average NA
+    float average_NA = total_NA / num_measurements;
 
-   
+    // Output NA for each measurement
+    printf("\nNumerical Aperture (NA) for each measurement:\n");
+    for (int i = 0; i < num_measurements; i++) {
+        printf("Measurement %d: %f\n", i + 1, NA[i]);
+    }
+
+    printf("\nAverage Numerical Aperture (NA) for the given measurements: %f\n\n", average_NA);
+
+    return 0;
 }
